e67faac8709ccc0ce5d2026e7a375069441d5ac8
zeroornull <xxpfantastic@yahoo.com>
2026-08-17 21:36:04 +0800

    Update TypeScript configuration and import path in Vite config
    - Added "noEmit" and "allowImportingTsExtensions" options to tsconfig.json for improved TypeScript handling.
    - Updated import path for mock API in vite.config.ts to include the .ts extension for consistency.

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Components from 'unplugin-vue-components/vite'
 import { VantResolver } from 'unplugin-vue-components/resolvers'
-import { mockApi } from './mock/api'
+import { mockApi } from './mock/api.ts'
 
 // https://vite.dev/config/
 export default defineConfig({
